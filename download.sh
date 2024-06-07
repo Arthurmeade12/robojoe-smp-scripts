0f5fb05f06afe90b8a49c226237c011d5259d616
Arthurmeade12 <arthurmeade12@gmail.com>
2024-06-07 14:20:34 -0500

    More work on download.sh

--- download.sh
+++ download.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-set -u
+set -ux
 TARGET_DIR="${TARGET_DIR:="$(dirname "${0}")"}"
 CURL_ARGS='-JlOf#' # This variable is unquoted when expanded; word splitting will occur
 
 SPIGOT=(
-  'Skoice'
   'GravestonesPlus'
-  'MyWorlds'
-)
-BROKEN=(
-  'HarderRespawn'
-  'GrimAC'
-  'Terra'
-  'Vault'
+  'GrimAC' # Has Hangar too
+  'HarderDespawn' # Hangar ONLY
+  #'Protocollib Stable'
+  #'MyWorlds Stable'
+  'Vault' # Has Curseforge too
 )
 GEYSER=(
   # Geyser project names
@@ -20,16 +17,20 @@ GEYSER=(
   'floodgate'
 )
 declare -A JENKINS=( # ['Jenkins base urls']='filename string to grep'
-  ['ci.dmulloy2.net/job/ProtocolLib']='' # Protocollib
+  ['ci.dmulloy2.net/job/ProtocolLib']='' # Protocollib Dev
   ['ci.lucko.me/job/LuckPerms']='bukkit/' # Luckperms
-  ['ci.ender.zone/job/EssentialsX']='jars/EssentialsX-' # EssentialsX
-  ['ci.ender.zone/job/EssentialsX']='jars/EssentialsXDiscord-' # EssentialsXDiscord
+  ['ci.ender.zone/job/EssentialsX']='jars/EssentialsX-' # EssentialsX Dev
+  ['ci.mg-dev.eu/job/BKCommonLib']='' # BKCommonLib Dev
 )
 MODRINTH=(
-  '9857f67f2fd1640bc4913a7e1781dfa8e167035c' # BKCommonLib
+  # '9857f67f2fd1640bc4913a7e1781dfa8e167035c' # BKCommonLib Stable
   '29b2bf30efaab24aac0c3f147fbe9d13fb63436d' # Chunky
+  '33b6c2b5dad6f5b99235d4883d4ac1a3d200c3d4' # ChunkyBorder
   'd7eec4b81240739ad6aec537ac42c772647b56de' # CoreProtect
   '7d59c830c5ea7683e0619f0524318e8ec1ef59c7' # Craftbook
+  'f55005317a18ec583a64821cf310b91d8f475c15' # DiscordSRV
+  'd2cd2591ea5af93a17f41d25068a6fb3247ac4c6' # DriveBackupV2
+  '02bf45c4d531b6c644515091ef0d5e4b751f2852' # GriefPreventions
   #'bf503af2778cafe8621d5e3ba67ded95ca034058' # Maintenance # Author kennytv has not put the latest version on modrinth
   'fea27f4ca32dd777ba82992d2c0cdfb8598f07b7' # Mclo.gs
   '2e5e2baa7ac26b53d334d25771b87716adae4e8c' # Pl3xMap
@@ -92,7 +93,7 @@ do
   curl ${CURL_ARGS} "https://${JENKIN}/lastSuccessfulBuild/artifact/${FILE}"
 done
 
-echo "Plugins to update manually: ${SPIGOT[*]} ${BROKEN[*]}"
+echo "Plugins to update manually: ${SPIGOT[*]}"
 
 #TODO: Hangar (Where are the api docs?)
 
