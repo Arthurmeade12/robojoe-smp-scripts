3cf89f5cb744fcde9bc616a8ae3bdf2b1d2b9988
Arthurmeade12 <arthurmeade12@gmail.com>
2024-10-22 17:43:34 -0500

    Update

--- download.sh
+++ download.sh
@@ -2,7 +2,8 @@
 set -u
 WHEREAMI="$(dirname "${0}")"
 TARGET_DIR="${TARGET_DIR:="${WHEREAMI}"}"
-CURL_ARGS='-JlOf#' # This variable is unquoted when expanded; word splitting will occur
+#shellcheck disable=SC2086 # Word splitting intended for CURL_ARGS
+CURL_ARGS='-JlOf# --clobber'
 
 declare -A UNAVAILABLE=(
   # Spigot
@@ -18,27 +19,27 @@ declare -A JENKINS=( # ['Jenkins base urls']='filename string to grep'
   ['ci.ender.zone/job/EssentialsX']='jars/EssentialsX-' # EssentialsX Dev
 )
 declare -A MODRINTH=(
-  ['BKCommonLib (Stable)']='7c018de6db70bcb81cf0312b4e6a158d983c9422'
-  ['Chunky']='29b2bf30efaab24aac0c3f147fbe9d13fb63436d'
-  ['Chunky Border']='33b6c2b5dad6f5b99235d4883d4ac1a3d200c3d4'
-  ['CoreProtect']='d7eec4b81240739ad6aec537ac42c772647b56de'
-  ['Craftboook']='7d59c830c5ea7683e0619f0524318e8ec1ef59c7' #
-  ['DiscordSRV']='f55005317a18ec583a64821cf310b91d8f475c15' #
-  ['DriveBackupV2']='d2cd2591ea5af93a17f41d25068a6fb3247ac4c6' #
+  ['BKCommonLib (Stable)']='7c018de6db70bcb81cf0312b4e6a158d983c9422' # 10/20/24
+  ['Chunky']='b9366f80cf0045cbb791ba7e11e00f608b98d012' # 10/20/24
+  ['Chunky Border']='33b6c2b5dad6f5b99235d4883d4ac1a3d200c3d4' # 10/20/24
+  ['CoreProtect']='0613b4537bfaed09ca3ef93883abbb9f10c390f1' # 10/20/24
+  ['Craftbook']='9e2ddfdeb9640cd201054d7078ce8ca49a1feff5' # 10/20/24
+  ['DiscordSRV']='7ecbc8662b2681a33ad68ffa0858a9bceba655c7' # 10/20/24
+  ['DriveBackupV2']='270296e8c60de29c17bdf04fc3060df95d842aa7' # 10/20/24
   ['Geyser']='dff59953beedd93a5591f32db573db76dbbcf6ad' # 10/20/24
-  ['GriefPrevention']='02bf45c4d531b6c644515091ef0d5e4b751f2852' #
-  ['Grim Anticheat']='c27189490370a002e54ab399ad998309432e3514' #
-  ['Maintenance']='bf503af2778cafe8621d5e3ba67ded95ca034058' #
-  ['mclo.gs']='fea27f4ca32dd777ba82992d2c0cdfb8598f07b7' #
+  ['GriefPrevention']='1b6bf6bc24cdaf21d1e40cc4cfac99f0701bc4dd' # 10/20/24
+  ['Grim Anticheat']='c27189490370a002e54ab399ad998309432e3514' # 10/20/24
+  ['Maintenance']='ba93789d9bbbc1dd8d58333ff6e6b7ac1f015b9f' # 10/20/24
+  ['mclo.gs']='fea27f4ca32dd777ba82992d2c0cdfb8598f07b7' # 10/20/24
   ['Pl3xMap']='da39a3ee5e6b4b0d3255bfef95601890afd80709' #
   ['Simple Voice Chat']='3745b56d2a8c15d98db68a449969d3690cfdb308' # 10/20/24
   ['Simple Voice Chat Discord Bridge']='33560ae663380366ab2d353118d6d39e58ec56d3' # 10/20/24
-  ['ViaBackwards']='2b308cbae2ffaa50be612b51d80bd91a7341b65c' #
-  ['ViaVersion']='13ae61a3a4beb3e7d00f6f7763136f336f58ff6d' #
+  ['ViaBackwards']='95af4be3c13aad778834dd2ed5e7447ea8f78485' # 10/20/24
+  ['ViaVersion']='6680f915fc242bfc1fc847f54cd1056c74ec1b2d' # 10/20/24
 )
 
 msg(){
-  printf '\033[;1;32m ==> \033[0;m\033[;1m%s : \033[:0m' "${*}"
+  printf '\033[;1;32m ==> \033[0;m\033[;1m%s : \033[:0m\n' "${*}"
 }
 
 PAYLOAD='{
@@ -55,7 +56,9 @@ PAYLOAD='{
 }
 '
 # 1.20 for mclo.gs
-# Paper needed by more plugins than not (even though we run purpur)
+# 1.20.6 for CoreProtect
+# 1.21 for Craftbook, Simple Voice Chat Discord Bridge
+# Paper for Chunky, Chunky Border, Geyser, GriefPrevention (maybe? inconsistent), Grim Anticheat, Maintenance, ViaBackwards, ViaVersion
 
 ### Code:
 
@@ -67,6 +70,8 @@ then
 fi
 
 # Purpur
+msg 'Purpur'
+#shellcheck disable=SC2086 # Word splitting intended for CURL_ARGS
 curl ${CURL_ARGS} 'https://api.purpurmc.org/v2/purpur/1.21.1/latest/download'
 
 # Plugin folder check
@@ -78,7 +83,8 @@ pushd ./plugins
 for PROJECT in "${!MODRINTH[@]}"
 do
   msg "${PROJECT}"
-  curl -sX POST "https://api.modrinth.com/v2/version_file/${MODRINTH["${PROJECT}"]}}/update" -H "Content-Type: application/json" --data-binary "${PAYLOAD}" | \
+  #shellcheck disable=SC2086 # Word splitting intended for CURL_ARGS
+  curl -sX POST "https://api.modrinth.com/v2/version_file/${MODRINTH["${PROJECT}"]}/update" -H "Content-Type: application/json" --data-binary "${PAYLOAD}" | \
     jq -r '.files[].url' | \
     xargs curl ${CURL_ARGS}
 done
@@ -86,8 +92,7 @@ done
 # Floodgate
 APIURL="https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest"
 msg 'Floodgate'
-curl -#L -o "$(curl --silent -L "https://download.geysermc.org/v2/projects/${PROJECT}/versions/latest/builds/latest" |\
-jq -r .downloads.spigot.name)" "${APIURL}"/downloads/spigot
+curl -#L -o 'floodgate-spigot.jar' "${APIURL}/downloads/spigot"
 
 # Jenkins
 for JENKIN in "${!JENKINS[@]}"
@@ -95,6 +100,7 @@ do
   msg "${JENKIN}"
   FILE="$(curl -s "https://${JENKIN}/lastSuccessfulBuild/api/json" | \
     jq -r '.artifacts[].relativePath' | grep "${JENKINS["${JENKIN}"]}")"
+  #shellcheck disable=SC2086 # Word splitting intended for CURL_ARGS
   curl ${CURL_ARGS} "https://${JENKIN}/lastSuccessfulBuild/artifact/${FILE}"
 done
 
@@ -102,7 +108,7 @@ done
 popd
 
 echo "Plugins to update manually: ${!UNAVAILABLE[*]}"
-echo -n "Automatically open their URLs ? (y/n) : "
+echo -n 'Automatically open their URLs ? (y/n) : '
 read -n 1 ANSWER
 if [[ "${ANSWER}" = 'y' ]] || [[ "${ANSWER}" = 'Y' ]]
 then
@@ -111,5 +117,5 @@ then
     xdg-open "${UNAVAILABLE["${LINK}"]}"
   done
 fi
-printf '%s\n' "Date of last run: $(cat "${WHEREAMI}/.timestamp")"
+printf '\n%s\n' "Date of last run: $(cat "${WHEREAMI}/.timestamp")"
 date > "${WHEREAMI}/.timestamp"
